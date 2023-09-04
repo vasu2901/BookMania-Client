b933db0e7119866c2fafd9e6f26bfb3d15049486
Vighnesh Vasu Vats <96187804+vasu2901@users.noreply.github.com>
2023-09-04 20:44:40 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,7 +7,7 @@ const Signup = () => {
   let history = useNavigate();
   const check = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://balkanid-server-snw1.onrender.com/api/register", {
+    const response = await fetch("http://localhost:3000/api/register", {
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
