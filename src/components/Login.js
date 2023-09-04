582d9c4cb4826dcf459d4a2a069fed1ee8431986
Vighnesh Vasu Vats <96187804+vasu2901@users.noreply.github.com>
2023-09-04 20:17:21 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = () => {
     let history = useNavigate();
     const user = async (e) => {
         e.preventDefault();
-        const response = await fetch("https://balkanid-server-snw1.onrender.com/api/login", {
+        const response = await fetch("http://127.0.0.1:3000/api/login", {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
