315b53d38dd22402a2bac5049512df91c96c2124
Vighnesh Vasu Vats <96187804+vasu2901@users.noreply.github.com>
2023-09-04 20:17:43 +0530

    Update Reverify.js

--- src/components/Reverify.js
+++ src/components/Reverify.js
@@ -7,7 +7,7 @@ export const Reverify = () => {
     const history = useNavigate()
     const verify = async (e) => {
         e.preventDefault()
-        const response = await fetch("https://balkanid-server-snw1.onrender.com/api/reverify", {
+        const response = await fetch("http://127.0.0.1:3000/api/reverify", {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json',
